f2c26a157b7d1adbd77e711f38f26f8616987c8d
Celso de Sa <730584+CelsoDeSa@users.noreply.github.com>
2021-09-06 22:14:12 -0300

    Managed to make it less ugly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-       session[:user_id] = @user.id
-       redirect_to '/albums'
+    if logged_in?
+      redirect_to '/albums'
+    elsif @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/albums'
     else
        redirect_to '/login'
        flash[:alert] = "Sorry, we couldn't find an account with this username. Please check you're using the right username and try again."
@@ -18,6 +20,10 @@ class SessionsController < ApplicationController
   def login
   end
 
-  def welcome
+  def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to root_url
   end
+    
 end
